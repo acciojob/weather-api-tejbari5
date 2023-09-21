80b88e8a942912a1dee441f2ed1805e85c0c9ae9
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-09-21 10:47:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const weather = document.getElementById("weather");
+weatherData.addEventListener("click",(e){
+	e.prevenetDeafault();
+	fetch("https://openweathermap.org/api").then((res)=>{
+		res.json()
+	})
+	.then((data)=>{
+		console.log(data);
+	}).catch((err)=>{
+		console.log(err);
+	})
+})
